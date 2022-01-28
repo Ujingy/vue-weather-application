d5dcd0c4b7e27c27410ff03201628d6a359510fa
Ujingy <hyjing00@gmail.com>
2022-01-29 01:24:24 +0900

    first commit

---
+++ src/components/weather/CitySelector.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <ul class="city-selector">
+      <li
+        @click="selectCity(city)"
+        :class="{ isSelected: city.selected }"
+        v-for="city in cities"
+        :key="city.code"
+      >
+        {{ city.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CitySelector",
+  data() {
+    return {
+      cities: [
+        { code: "Seoul", label: "서울", selected: false },
+        { code: "Singapore", label: "싱가포르", selected: false },
+        { code: "London", label: "런던", selected: false },
+        { code: "Moscow", label: "모스크바", selected: false },
+        { code: "Yakutsk", label: "야크추크", selected: false },
+      ],
+    };
+  },
+  methods: {
+    selectCity(city) {
+      city.selected = !city.selected;
+      this.$emit("selectCity", city);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.city-selector {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+}
+.city-selector li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 0.05em solid #efefef;
+  padding: 0.3em;
+  cursor: pointer;
+}
+.isSelected {
+  background: #23bd9b;
+  color: #fff;
+}
+</style>
